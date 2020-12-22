44814c1fe6a6dbdf6bd324fb4a2975a3ab379431
JeremyAndress <jeremysilvasilva@gmail.com>
2020-12-22 12:03:02 -0300

    FIX: yarn install

--- install_package/node.sh
+++ install_package/node.sh
@@ -3,8 +3,10 @@ apt-get --yes install nodejs
 
 apt-get --yes install npm
 
-apt-get --yes install yarn
-
 mkdir -p  /usr/local/lib/node_modules/
 
 chown -R $USER /usr/local/lib/node_modules
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+apt-get install yarn -y
